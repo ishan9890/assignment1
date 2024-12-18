7361127502a608b13eb2a86e3dede3f8357f8e48
Ishan <ishanjoshi234@gmail.com>
2024-12-18 15:15:47 +0545

    assignment

--- Assignment2/1q.py
+++ Assignment2/1q.py
@@ -1,1 +1,6 @@
-a = 12
+a = input("Enter the value of a: ")
+b = input("Enter the value of b: ")
+a,b=b,a
+print("\nAfter Swapping")
+print(f"The value of a is {a}")
+print(f"The value of b is {b}")
