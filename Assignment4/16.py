e1e608f8b14b9b7e976e718ddc3a0f08fc709052
Ishan <ishanjoshi234@gmail.com>
2024-12-24 18:21:55 +0545

    .

---
+++ Assignment4/16.py
@@ -0,0 +1,6 @@
+n = [1,2,3,4,5,6,7,8,9]
+e = []
+for i in n:
+    if i % 2 == 0:
+        e.append(i)
+print(f"The even numbers are: {e}")
---
+++ Assignment4/17.py
@@ -0,0 +1,6 @@
+n = [12,13,14,15,16,17,18,19]
+o = []
+for i in n:
+    if i % 2 != 0:
+        o.append(i)
+print(f"The even numbers are: {o}")
